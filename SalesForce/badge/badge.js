1ad8bad1d4a9c5edf998dd69e2bee8c05724f0e3
Rios <jrios@c21stores.com>
2020-03-05 17:15:43 -0500

    badge

---
+++ SalesForce/badge/badge.js
@@ -0,0 +1,32 @@
+[
+  {
+    "category": "hidden-great-gifts",
+    "text": "Great Gift!",
+    "cssClass": "badge-great-gift"
+  },
+  {
+    "category": "hidden-plus",
+    "text": "Plus",
+    "cssClass": "badge-great-gift"
+  },
+  {
+    "category": "hidden-new-arrivals",
+    "text": "New",
+    "cssClass": "badge-new-arrivals"
+  },
+  {
+    "category": "hidden-best-sellers",
+    "text": "Best Seller",
+    "cssClass": "badge-best-seller"
+  },
+  {
+    "category": "hidden-wide",
+    "text": "wide",
+    "cssClass": "badge-great-gift"
+  },
+  {
+    "category": "hidden-luxury",
+    "text": "Luxury",
+    "cssClass": "badge-luxury"
+  }
+]
